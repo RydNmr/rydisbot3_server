b0562b1322a1bd17d5a3b14becf24c0b87763df7
RydNmr <rydnmr@gmail.com>
2024-02-13 20:59:45 +0100

    cors edited

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const {Server} = require("socket.io");
 const io = new Server(server, {
     cors: {
-      origin: ["http://192.168.1.54:3000", "http://localhost:3000"]
+      origin: ["https://rydisbot3-clinet.vercel.app"]
     }
   });
 const PORT = 5000;
